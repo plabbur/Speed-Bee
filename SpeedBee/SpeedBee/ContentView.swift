a518609598b3a905b9b36524d3b59e3c3c352d71
Cole Abrams <coleabrams11@gmail.com>
2024-02-14 12:43:45 -0800

    first commit

---
+++ SpeedBee/SpeedBee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedBee
+//
+//  Created by Cole Abrams on 2/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedBee/SpeedBee/SpeedBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedBeeApp.swift
+//  SpeedBee
+//
+//  Created by Cole Abrams on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeedBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
